2b5679add16fb2becf2a75cb00adf3c9bf20e432
nronne <nikolajron@gmail.com>
2025-01-29 10:33:11 +0100

    updated cli

--- src/agedi/cli/sample.py
+++ src/agedi/cli/sample.py
@@ -9,39 +9,71 @@ from ase import Atoms
 from ase.io import read, write
 
 from agedi.cli.train import get_package, get_conditioning, get_noisers
-from agedi.diffusion import Diffusion
+
+from agedi import Diffusion
 from agedi.models import ScoreModel
 from agedi.data import Dataset, AtomsGraph
 
 
-click.rich_click.OPTION_GROUPS.update({
-    "agedi sample": [
-        {"name": "Model Options", "options": ['path']},
-        {"name": "Structure Options", "options": ['--n_samples', '--n_atoms', '--formula', '--cell', '--template_path', '--confinement']},
-        {"name": "Sampling Hyperparameters", "options": ['--output', '--name', '--steps', '--seed', '--eps', '--batch_size']},
-    ]
-})
+click.rich_click.OPTION_GROUPS.update(
+    {
+        "agedi sample": [
+            {"name": "Model Options", "options": ["path"]},
+            {
+                "name": "Structure Options",
+                "options": [
+                    "--n_samples",
+                    "--n_atoms",
+                    "--formula",
+                    "--cell",
+                    "--template_path",
+                    "--confinement",
+                ],
+            },
+            {
+                "name": "Sampling Hyperparameters",
+                "options": [
+                    "--output",
+                    "--name",
+                    "--steps",
+                    "--seed",
+                    "--eps",
+                    "--batch_size",
+                ],
+            },
+        ]
+    }
+)
+
 
 @click.command()
 @click.argument("path", type=click.Path(exists=True))
-@click.option("--n_samples", '-n', type=int, show_default=True, default=12)
-@click.option("--seed", '-s', type=int, show_default=True, default=42)
+@click.option("--n_samples", "-n", type=int, show_default=True, default=12)
+@click.option("--seed", "-s", type=int, show_default=True, default=42)
 @click.option("--steps", type=int, show_default=True, default=500)
 @click.option("--eps", type=float, show_default=True, default=0.005)
-@click.option("--batch_size", '-b', show_default=True, type=int, default=64)
-@click.option("--output", '-o', type=click.Path(), show_default=True, default=".")
+@click.option("--batch_size", "-b", show_default=True, type=int, default=64)
+@click.option("--output", "-o", type=click.Path(), show_default=True, default=".")
 @click.option("--name", type=str, show_default=True, default="sampled")
-@click.option("--n_atoms", '-a', type=int)
-@click.option("--formula", '-f', type=str)
-@click.option("--cell", '-c', nargs=9, type=float)
-@click.option("--template_path", '-t', type=click.Path(exists=True))
-@click.option('--confinement', nargs=2, type=float, default=None, help='Z-confinement to use for the data. Give min and max value')
-@click.option('--progress_bar', is_flag=True, help='Show progress bar')
-@click.option('--save_path', is_flag=True, help='Save entire diffusion trajectory pathway')
+@click.option("--n_atoms", "-a", type=int)
+@click.option("--formula", "-f", type=str)
+@click.option("--cell", "-c", nargs=9, type=float)
+@click.option("--template_path", "-t", type=click.Path(exists=True))
+@click.option(
+    "--confinement",
+    nargs=2,
+    type=float,
+    default=None,
+    help="Z-confinement to use for the data. Give min and max value",
+)
+@click.option("--progress_bar", is_flag=True, help="Show progress bar")
+@click.option(
+    "--save_path", is_flag=True, help="Save entire diffusion trajectory pathway"
+)
 def sample(path, **kwargs):
     click.echo(f"Loading model from: {path}")
     # read yaml file
-    with open(Path(path) / 'hparams.yaml', "r") as file:
+    with open(Path(path) / "hparams.yaml", "r") as file:
         params = yaml.safe_load(file)
 
     sample_kwargs = {
@@ -54,7 +86,7 @@ def sample(path, **kwargs):
         "save_path": kwargs["save_path"],
         "confinement": kwargs["confinement"],
     }
-        
+
     if kwargs["template_path"]:
         t = read(kwargs["template_path"])
         sample_kwargs["cell"] = np.array(t.cell)
@@ -62,25 +94,34 @@ def sample(path, **kwargs):
         if kwargs["confinement"]:
             template.confinement = torch.tensor(kwargs["confinement"]).reshape(1, 2)
         sample_kwargs["template"] = template
-        
-                      
-    if kwargs['n_atoms']:
+
+    if kwargs["n_atoms"]:
         sample_kwargs["n_atoms"] = kwargs["n_atoms"]
 
-    if kwargs['formula']:
-        a = Atoms(kwargs['formula'])
+    if kwargs["formula"]:
+        a = Atoms(kwargs["formula"])
         sample_kwargs["n_atoms"] = len(a)
         sample_kwargs["atomic_numbers"] = a.get_atomic_numbers()
 
-    if sample_kwargs.get('cell') is None:
+    if sample_kwargs.get("cell") is None:
         sample_kwargs["cell"] = np.array(params["cell"]).reshape(3, 3)
-
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # Model
-    translator, representation, heads = get_package(params['model'], params['cutoff'], params['noisers'], params['feature_size'], params['n_blocks'])
-    conditionings = get_conditioning(params['conditioning'])
-    noisers = get_noisers(params['noisers'], params['noiser_sdes'], params['noiser_distributions'], params['prior_distributions'])
+    translator, representation, heads = get_package(
+        params["model"],
+        params["cutoff"],
+        params["noisers"],
+        params["feature_size"],
+        params["n_blocks"],
+    )
+    conditionings = get_conditioning(params["conditioning"])
+    if kwargs["confinement"] is not None and "position" in params["noisers"]:
+        confined = True
+    else:
+        confined = False
+
+    noisers = get_noisers(params["noisers"], confined=confined)
 
     score_model = ScoreModel(
         translator=translator,
@@ -88,27 +129,34 @@ def sample(path, **kwargs):
         conditionings=conditionings,
         heads=[h.to(device) for h in heads],
     )
-    
+
     diffusion = Diffusion(
         score_model,
         noisers,
-        optim_config={"lr": params['lr']},
-        scheduler_config={"factor": params['lr_factor'], "patience": params['lr_patience']},
+        optim_config={"lr": params["lr"]},
+        scheduler_config={
+            "factor": params["lr_factor"],
+            "patience": params["lr_patience"],
+        },
     ).to(device)
 
-
-    diffusion.load_state_dict(torch.load(Path(path) / 'checkpoints/best_model.ckpt', weights_only=True, map_location=device)['state_dict'])
+    diffusion.load_state_dict(
+        torch.load(
+            Path(path) / "checkpoints/best_model.ckpt",
+            weights_only=True,
+            map_location=device,
+        )["state_dict"]
+    )
 
     diffusion.eval()
 
     with torch.no_grad():
         graph_list = diffusion.sample(**sample_kwargs)
 
-
     Path(kwargs["output"]).mkdir(parents=True, exist_ok=True)
     name = kwargs["name"]
-        
-    if sample_kwargs.get('save_path', False):
+
+    if sample_kwargs.get("save_path", False):
         for i, graph_list_i in enumerate(graph_list):
             atoms_list = [g.to_atoms() for g in graph_list_i]
             write(Path(kwargs["output"]) / f"{name}_{i}.traj", atoms_list)
@@ -116,4 +164,3 @@ def sample(path, **kwargs):
     else:
         atoms_list = [g.to_atoms() for g in graph_list]
         write(Path(kwargs["output"]) / f"{name}.traj", atoms_list)
-
--- src/agedi/cli/train.py
+++ src/agedi/cli/train.py
@@ -11,62 +11,170 @@ from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
 
 from ase.io import read
 
-from agedi.diffusion import Diffusion
+from agedi import Diffusion
 from agedi.models import ScoreModel
 from agedi.data import Dataset
 
-click.rich_click.OPTION_GROUPS.update({
-    "agedi train": [
-        {"name": "Score Model Options", "options": ['--model', '--cutoff', '--feature_size', '--n_blocks']},
-        {"name": "Diffusion Model Options", "options": ['--noisers', '--noiser_sdes', '--noiser_distributions', '--prior_distributions', '--condition']}, # , '--noiser_sde_kwargs'
-        {"name": "Training Options", "options": ['--epochs', '--time', '--lr', '--batch_size', '--lr_patience', '--lr_factor', '--progress-bar']},
-        {"name": "Data Options", "options": ['--mask', '--confinement']},
-        {"name": "Logging Options", "options": ['--log_dir', '--log_interval']},
-    ]
-})
-
-# @click.option('--noiser_sde_kwargs' , type=click.Choice(["VE", "VP"]), default=["VP"], multiple=True, show_default=True, help='type of SDE for each noiser')
+click.rich_click.OPTION_GROUPS.update(
+    {
+        "agedi train": [
+            {
+                "name": "Score Model Options",
+                "options": ["--model", "--cutoff", "--feature_size", "--n_blocks"],
+            },
+            {
+                "name": "Diffusion Model Options",
+                "options": ["--noisers", "--condition"],
+            },  
+            {
+                "name": "Training Options",
+                "options": [
+                    "--epochs",
+                    "--time",
+                    "--lr",
+                    "--batch_size",
+                    "--lr_patience",
+                    "--lr_factor",
+                    "--progress-bar",
+                ],
+            },
+            {"name": "Data Options", "options": ["--mask", "--confinement"]},
+            {"name": "Logging Options", "options": ["--log_dir", "--log_interval"]},
+        ]
+    }
+)
 
 @click.command()
-@click.argument('data', type=click.Path(exists=True))
-@click.option('--model', '-m', type=click.Choice(['PaiNN']), default="PaiNN", show_default=True, help='Representation to use for the model')
-@click.option('--cutoff', '-r', type=float, default=6.0, show_default=True, help='Cutoff for the representation in Å')
-@click.option('--feature_size', '-f', type=int, default=64, show_default=True, help='Feature size for the representation')
-@click.option('--n_blocks', '-b', type=int, default=4, show_default=True, help='Number of blocks for the representation')
-@click.option('--noisers', "-n" , type=click.Choice(["positions", "types", 'cell']), default=["positions"], multiple=True, show_default=True, help='type of heads to use')
-@click.option('--noiser_sdes' , type=click.Choice(["VE", "VP"]), default=["VE"], multiple=True, show_default=True, help='type of SDE for each noiser')
-@click.option('--noiser_distributions' , type=click.Choice(["Normal", "TruncatedNormal", "WrappedNormal"]),
-              default=["Normal"], multiple=True, show_default=True, help='Noise distribution for each noiser')
-@click.option('--prior_distributions' , type=click.Choice(["UniformCell", "UniformCellConfined", "NormalConfined"]),
-              default=["UniformCell"], multiple=True, show_default=True, help='Prior distribution for each noiser')
-@click.option('--conditioning', '-c', type=click.Choice(['none']), default='none', help='type of conditionings to use', hidden=True)
-@click.option('--epochs', '-e', type=int, default=1e6, show_default=True, help='Number of epochs to train for')
-@click.option('--time', '-t', type=int, default=1440, help='Time to train for in minutes')
-@click.option('--lr', type=float, default=1e-4, show_default=True, help='Learning rate')
-@click.option('--batch_size', '-b', type=int, default=32, show_default=True, help='Batch size')
-@click.option('--lr_patience', type=int, default=100, show_default=True, help='Number of epochs to wait before reducing the learning rate')
-@click.option('--lr_factor', type=float, default=0.98, show_default=True, help='Factor to reduce the learning rate by')
-@click.option('--mask', type=click.Choice(['MaskFixed', 'none']), default='none', help='Masking to use for the data')
-@click.option('--confinement', nargs=2, type=float, default=None, help='Z-confinement to use for the data. Give min and max value')
-@click.option('--log_dir', type=click.Path(), default='logs', show_default=True, help='Directory to save logs to')
-@click.option('--log_interval', type=int, default=10, show_default=True, help='Interval to log at')
-@click.option('--progress_bar', is_flag=True, help='Show progress bar')
-def train(
-        **params
-):
-    print("AGeDi Training Diffusion Model")
-    print('-'*30)
-    print('Options:')
-    for key, value in params.items():
-        print(f'{key}: {value}')
-
-    params['data'] = str(Path(params['data']).resolve())
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+@click.argument("data", type=click.Path(exists=True))
+@click.option(
+    "--model",
+    "-m",
+    type=click.Choice(["PaiNN"]),
+    default="PaiNN",
+    show_default=True,
+    help="Representation to use for the model",
+)
+@click.option(
+    "--cutoff",
+    "-r",
+    type=float,
+    default=6.0,
+    show_default=True,
+    help="Cutoff for the representation in Å",
+)
+@click.option(
+    "--feature_size",
+    "-f",
+    type=int,
+    default=64,
+    show_default=True,
+    help="Feature size for the representation",
+)
+@click.option(
+    "--n_blocks",
+    "-b",
+    type=int,
+    default=4,
+    show_default=True,
+    help="Number of blocks for the representation",
+)
+@click.option(
+    "--noisers",
+    "-n",
+    type=click.Choice(["positions", "types", "cell"]),
+    default=["positions"],
+    multiple=True,
+    show_default=True,
+    help="type of heads to use",
+)
+@click.option(
+    "--conditioning",
+    "-c",
+    type=click.Choice(["none"]),
+    default="none",
+    help="type of conditionings to use",
+    hidden=True,
+)
+@click.option(
+    "--epochs",
+    "-e",
+    type=int,
+    default=1e6,
+    show_default=True,
+    help="Number of epochs to train for",
+)
+@click.option(
+    "--time", "-t", type=int, default=1440, help="Time to train for in minutes"
+)
+@click.option("--lr", type=float, default=1e-4, show_default=True, help="Learning rate")
+@click.option(
+    "--batch_size", "-b", type=int, default=32, show_default=True, help="Batch size"
+)
+@click.option(
+    "--lr_patience",
+    type=int,
+    default=100,
+    show_default=True,
+    help="Number of epochs to wait before reducing the learning rate",
+)
+@click.option(
+    "--lr_factor",
+    type=float,
+    default=0.98,
+    show_default=True,
+    help="Factor to reduce the learning rate by",
+)
+@click.option(
+    "--mask",
+    type=click.Choice(["MaskFixed", "none"]),
+    default="none",
+    help="Masking to use for the data",
+)
+@click.option(
+    "--confinement",
+    nargs=2,
+    type=float,
+    default=None,
+    help="Z-confinement to use for the data. Give min and max value",
+)
+@click.option(
+    "--log_dir",
+    type=click.Path(),
+    default="logs",
+    show_default=True,
+    help="Directory to save logs to",
+)
+@click.option(
+    "--log_interval", type=int, default=10, show_default=True, help="Interval to log at"
+)
+@click.option("--progress_bar", is_flag=True, help="Show progress bar")
+def train(**params):
     
+    print("AGeDi Training Diffusion Model")
+    print("-" * 30)
+    print("Options:")
+    for key, value in params.items():
+        print(f"{key}: {value}")
+
+    params["data"] = str(Path(params["data"]).resolve())
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     # Model
-    translator, representation, heads = get_package(params['model'], params['cutoff'], params['noisers'], params['feature_size'], params['n_blocks'])
-    conditionings = get_conditioning(params['conditioning'])
-    noisers = get_noisers(params['noisers'], params['noiser_sdes'], params['noiser_distributions'], params['prior_distributions'])
+    translator, representation, heads = get_package(
+        params["model"],
+        params["cutoff"],
+        params["noisers"],
+        params["feature_size"],
+        params["n_blocks"],
+    )
+    conditionings = get_conditioning(params["conditioning"])
+
+    if params["confinement"] is not None and "positions" in params["noisers"]:
+        confined = True
+    else:
+        confined = False
+
+    noisers = get_noisers(params["noisers"], confined=confined)
 
     score_model = ScoreModel(
         translator=translator,
@@ -74,24 +182,29 @@ def train(
         conditionings=conditionings,
         heads=[h.to(device) for h in heads],
     )
-    
+
     diffusion = Diffusion(
         score_model,
         noisers,
-        optim_config={"lr": params['lr']},
-        scheduler_config={"factor": params['lr_factor'], "patience": params['lr_patience']},
+        optim_config={"lr": params["lr"]},
+        scheduler_config={
+            "factor": params["lr_factor"],
+            "patience": params["lr_patience"],
+        },
     )
 
     # Data
-    data = read(params['data'], ":")
-    dataset = Dataset(cutoff=params['cutoff'], batch_size=params['batch_size'])
+    data = read(params["data"], ":")
+    dataset = Dataset(cutoff=params["cutoff"], batch_size=params["batch_size"])
     click.echo(f"Loaded dataset with {len(data)} samples")
-    dataset.add_atoms_data(data, mask_method=params['mask'], confinement=params['confinement'])
+    dataset.add_atoms_data(
+        data, mask_method=params["mask"], confinement=params["confinement"]
+    )
     dataset.setup()
 
     # Training
-    logger = TensorBoardLogger(save_dir=params['log_dir'], name='')
-    params['log_dir'] = str(Path(logger.log_dir).resolve())
+    logger = TensorBoardLogger(save_dir=params["log_dir"], name="")
+    params["log_dir"] = str(Path(logger.log_dir).resolve())
     log_hparams = params | data_info(data)
     logger.log_hyperparams(log_hparams, {"val_loss": 0})
 
@@ -115,13 +228,13 @@ def train(
     trainer = Trainer(
         accelerator="auto",
         devices=1,
-        max_epochs=params['epochs'],
-        max_time={"minutes": params['time']} if params['time'] is not None else None,
+        max_epochs=params["epochs"],
+        max_time={"minutes": params["time"]} if params["time"] is not None else None,
         logger=logger,
         callbacks=callbacks,
         gradient_clip_val=1.0,
-        enable_progress_bar=params['progress_bar'],
-        log_every_n_steps=params['log_interval'],
+        enable_progress_bar=params["progress_bar"],
+        log_every_n_steps=params["log_interval"],
         inference_mode=False,
     )
 
@@ -129,62 +242,54 @@ def train(
 
     print("To sample from model use: ")
     print(f"agedi sample {params['log_dir']} -f ...")
-    
-def get_noisers(noisers, sdes, distributions, priors):
-    from agedi.diffusion.noisers import PositionsNoiser
-    from agedi.diffusion.noisers import VP, VE
-    from agedi.diffusion.noisers import Normal, TruncatedNormal, WrappedNormal, UniformCell, UniformCellConfined, NormalConfined
+
+
+def get_noisers(noisers, confined=False):
+    from agedi.diffusion.noisers import PositionsNoiser, TypesNoiser
+    from agedi.diffusion.distributions import Normal, UniformCellConfined
+
     noiser_list = []
-    for noiser, sde, dist, prior in zip(noisers, sdes, distributions, priors):
-        match sde:
-            case "VE":
-                sde = VE
-            case "VP":
-                sde = VP
-            case _:
-                raise ValueError(f"Unknown SDE {sde}")
-        match dist:
-            case "Normal":
-                dist = Normal()
-            case "TruncatedNormal":
-                dist = TruncatedNormal()
-            case "WrappedNormal":
-                dist = WrappedNormal()
-            case _:
-                raise ValueError(f"Unknown Distribution {dist}")
-        match prior:
-            case "UniformCell":
-                prior = UniformCell()
-            case "UniformCellConfined":
-                prior = UniformCellConfined()
-            case "NormalConfined":
-                prior = NormalConfined()
-            case _:
-                raise ValueError(f"Unknown Prior {prior}")
+    for noiser in noisers:
         match noiser:
-            case 'positions':
-                noiser_list.append(PositionsNoiser(sde_class=sde, distribution=dist, prior=prior))
+            case "positions":
+                if confined:
+                    distribution = UniformCellConfined()
+                else:
+                    distribution = Normal()
+                noiser_list.append(PositionsNoiser(distribution=distribution))
+
+            case "types":
+                noiser_list.append(TypesNoiser())
+
             case _:
-                raise ValueError(f'Unknown noiser {noiser}')
+                raise ValueError(f"Unknown noiser {noiser}")
 
     return noiser_list
-    
+
+
 def get_conditioning(condition):
     from agedi.models.conditionings import TimeConditioning
-    conditioning = [TimeConditioning(),]
+
+    conditioning = [
+        TimeConditioning(),
+    ]
     match condition:
-        case 'none':
+        case "none":
             pass
 
     return conditioning
-    
+
+
 def get_package(model, cutoff, heads, feature_size, n_blocks):
     match model:
-        case 'PaiNN':
+        case "PaiNN":
             import schnetpack as spk
 
-            from agedi.models.schnetpack import (PositionsScore,
-                                                 SchNetPackTranslator)
+            from agedi.models.schnetpack import (
+                PositionsScore,
+                TypesScore,
+                SchNetPackTranslator,
+            )
 
             input_modules = [
                 spk.atomistic.PairwiseDistances(),
@@ -203,38 +308,37 @@ def get_package(model, cutoff, heads, feature_size, n_blocks):
 
             for head in heads:
                 match head:
-                    case 'positions':
+                    case "positions":
                         h.append(PositionsScore())
+                    case "types":
+                        h.append(TypesScore())
                     case _:
-                        raise ValueError(f'Unknown head {head}')
-
+                        raise ValueError(f"Unknown head {head}")
 
         case _:
-            raise ValueError(f'Unknown model {model}')
-
+            raise ValueError(f"Unknown model {model}")
 
     return translator, representation, h
-    
+
+
 def data_info(data):
     elements = set()
 
-    out = {'cell': None}
+    out = {"cell": None}
     check_cell = True
     for d in data:
         elements.update(d.get_chemical_symbols())
         if check_cell:
             if d.cell is not None:
-                out['cell'] = np.array(d.cell)
+                out["cell"] = np.array(d.cell)
             else:
-                if not np.all(out['cell'] == d.cell):
+                if not np.all(out["cell"] == d.cell):
                     check_cell = False
-    out['cell'] = out['cell'].flatten().tolist() if out['cell'] is not None else None
-                    
+    out["cell"] = out["cell"].flatten().tolist() if out["cell"] is not None else None
+
     out |= {
         "symbols": list(elements),
         "n_training_data": len(data),
     }
-        
+
     return out
-
-
