fe4f69ea99969ecbcc4ac2902bb97d2e4c59e95a
Nikolaj Rønne <nronne@phys.au.dk>
2025-02-25 10:59:52 +0100

    fixed repeat transform for conditioned properties

--- src/agedi/data/transforms.py
+++ src/agedi/data/transforms.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Dict
 
 from torch_geometric.transforms import BaseTransform
 
@@ -6,12 +6,24 @@ from agedi.data import AtomsGraph
 
 
 class Repeat(BaseTransform):
-    def __init__(self, m: Tuple[int, int, int] = (1, 1, 1)):
+    def __init__(self, m: Tuple[int, int, int] = (1, 1, 1), property: Dict[str, str]={}):
         self.m = m
 
     def forward(self, data: AtomsGraph) -> AtomsGraph:
+        new_properties = {}
+        for key, val in property.items():
+            repeats = self.m.prod()
+            if val == 'node':
+                new_properties[key] = data.getattr(key).repeat(repeats, 1)
+            if val == 'graph':
+                new_properties[key] = data.getattr(key) * repeats
+
         atoms = data.to_atoms()
         atoms = atoms.repeat(self.m)
         repeated_data = AtomsGraph.from_atoms(atoms)
+        
+        for key, val in new_properties.items():
+            repeated_data.setattr(key, val)
+            
         return repeated_data
 
